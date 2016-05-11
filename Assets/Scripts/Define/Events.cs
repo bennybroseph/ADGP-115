65aaedfa3a8d9cfa392dff8a2624e4c5d42b5b5e
Benjamin Odom <bennybroseph@gmail.com>
2016-05-11 16:35:56 -0500

    Rotated Everything
    Up is now in the positive Y, down is now in the negative Y instead of Z.

--- Assets/Scripts/Define/Events.cs
+++ Assets/Scripts/Define/Events.cs
@@ -8,25 +8,25 @@ namespace Define
     {
         // Used for testing purposes
         Test,
-        //There are no parameters
+        // There are no parameters
         NewGame,
-        //There are no parameters
+        // There are no parameters
         LoadGame,
-        //There are no parameters
+        // There are no parameters
         Instructions,
-        //There are no parameters
+        // There are no parameters
         QuitGame,
-        //There are no parameters
+        // There are no parameters
         PauseGame,
-        //There are no paramters
+        // There are no paramters
         UnPauseGame,
         /// <param name="Skill Index"> int: which skill to upgrade</param>
         UpgradeSkill,
         /// <param name="Skill Index"> int: which skill to use</param>
         UseSkill,
-        //There are no parameters
+        // There are no parameters
         SpawnWave,
-        
+        // There are no parameters
         GameOver
     }
 
