2dcb4ae064a46efea0a0af13160becb648d92326
Andrew Miller <A_Miller729@yahoo.com>
2016-05-12 18:05:38 -0500

    Added functionality to change Player UI Health and Mana Bars

--- Assets/Scripts/Define/Events.cs
+++ Assets/Scripts/Define/Events.cs
@@ -31,6 +31,13 @@ namespace Define
         /// <param name="Skill Index"> int: which skill's cooldown changed</param>
         /// <param name="New Value"> float: new value of the skill's cooldown</param> 
         SkillCooldownChanged,
+
+        /// <param name="Unit"> IStats: which unit's health changed</param>
+        UnitHealthChanged,
+        /// <param name="Unit"> IStats: which unit's mana changed</param>
+        UnitManaChanged,
+        /// <param name="Unit"> IStats: which unit's level changed</param>
+        UnitLevelChanged
     }
 
 }
