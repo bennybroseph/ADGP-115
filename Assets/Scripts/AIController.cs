ed3949c568a3cb7a061c1fc0867b6b26d9e9b92c
Andrew Miller <A_Miller729@yahoo.com>
2016-05-16 15:37:31 -0500

    Added Random Spawn Points

--- Assets/Scripts/AIController.cs
+++ Assets/Scripts/AIController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using Library;
 using Units;
 using Event = Define.Event;
-using System;
 
 public class AIController : MonoSingleton<AIController>, IController
 {
@@ -17,7 +16,8 @@ public class AIController : MonoSingleton<AIController>, IController
     [SerializeField]
     private int m_WaveCounter;
     private List<IControlable> m_Controlables;
-
+    [SerializeField]
+    private Vector3 m_Variance;
 
     private void Awake()
     {
@@ -72,23 +72,41 @@ public class AIController : MonoSingleton<AIController>, IController
         if (m_Enemies.Count != 0)
             return;
 
-        Vector3 spawnPoint1 = new Vector3(-14.25f, 0.5f, 0f);
-        Vector3 spawnPoint2 = new Vector3(-0.50f, 0.5f, 7.50f);
-        Vector3 spawnPoint3 = new Vector3(-0f, 0.5f, -8.5f);
+        m_WaveCounter++;
 
-        m_SpawnPoints.Add(spawnPoint1);
-        m_SpawnPoints.Add(spawnPoint2);
-        m_SpawnPoints.Add(spawnPoint3);
+        if (m_SpawnPoints.Count == 0)
+        {
+            Vector3 spawnPoint1 = new Vector3(-14.25f, 0.5f, 0f);
+            Vector3 spawnPoint2 = new Vector3(-0.50f, 0.5f, 7.50f);
+            Vector3 spawnPoint3 = new Vector3(0f, 0.5f, -8.5f);
 
+            m_SpawnPoints.Add(spawnPoint1);
+            m_SpawnPoints.Add(spawnPoint2);
+            m_SpawnPoints.Add(spawnPoint3);
+        }
+
+        
+        
         foreach (Vector3 spawnPoint in m_SpawnPoints)
         {
-            GameObject newObject = Instantiate(m_EnemyPrefab);
-            newObject.transform.position = spawnPoint;
+            for (int i = 0; i < m_WaveCounter; i++)
+            {
+                float x = Random.Range(-m_Variance.x, m_Variance.x);
+                float z = Random.Range(-m_Variance.z, m_Variance.z);
 
-            m_Enemies.Add(newObject.GetComponent<IStats>());
+                GameObject newObject = Instantiate(m_EnemyPrefab);
+                newObject.transform.position = new Vector3(
+                    spawnPoint.x + x,
+                    spawnPoint.y,
+                    spawnPoint.z + z);
+
+                m_Enemies.Add(newObject.GetComponent<IStats>());
+            }
+
+
         }
-        m_WaveCounter++;
-        Publisher.self.Broadcast(Event.SpawnWave,m_WaveCounter);
+        Publisher.self.Broadcast(Event.SpawnWave, m_WaveCounter);
+
     }
 
     private void OnUnitDied(Event a_Event, params object[] a_Params)
